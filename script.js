0d584eb4c223dbbe7b7c344e55e01eee186123b9
Glitch (hi-cactus-friends) <none>
2020-05-27 11:24:16 +0000

    🍖🌶 Updated with Glitch - added setChollaText function and minor adjustments

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// stops conversation using X in lower-left corner
+// [feature] stops conversation using X in lower-left corner
 const stopIcon = document.getElementById("stop");
 var stopBubble;
 
@@ -10,7 +10,7 @@ stopIcon.addEventListener("click", function() {
 function stopChat() {
   let chatDivsAI = document.getElementsByClassName("bubble");
   let chatDivsReplies = document.getElementsByClassName("bubble-button");
-  var chatDivs =  [...chatDivsAI, ...chatDivsReplies]; // spread operator
+  var chatDivs = [...chatDivsAI, ...chatDivsReplies]; // spread operator
   for (let i = 0; i < chatDivs.length; i++) {
     let chatDivsStyle = chatDivs[i].style;
     chatDivsStyle.visibility = "hidden";
@@ -25,94 +25,66 @@ function stopChat() {
 function sayGoodbye() {
   stopBubble = document.createElement("div");
   stopBubble.classList.add("bubble", "say", "stop-app");
-  let stopBubbleText = document.createTextNode("Ok, I understand. Thanks for talking with Saguaro. Goodbye!");
+  let stopBubbleText = document.createTextNode(
+    "Ok, I understand. Thanks for talking with Saguaro. Goodbye!"
+  );
   stopBubble.appendChild(stopBubbleText);
   stopIcon.insertAdjacentElement("afterend", stopBubble);
 }
 
-// timing of cholla question ("1B2BA" in convo.js)
-const chollaQuestion = document.getElementById("chollaquestion");
-const jumpingCholla = document.getElementById("jumpingcholla");
-const caneCholla = document.getElementById("canecholla");
-const teddyBearCholla = document.getElementById("teddybearcholla");
-var chollaMonth;
-const chollaQuestionText = `It is now ${chollaMonth} and there are blooming flowers on some of these cacti. What do the flowers look like?`;
-const jumpingChollaText = "There are white and pink flowers and green fruit"; // June July August
-const caneChollaTextSpring = "There are magenta / purple blooms"; // April May June
-const caneChollaTextSummer = "There are no flowers, but there are long-lasting yellow fruits"; // July August
-const teddBearChollaText = "There are yellow-green flowers"; // April May June September
+// [feature] cholla question ("1B2BA" in convo.js)
+function setChollaText() {
+  let chollaQuestion = document.getElementById("chollaquestion");
+  let jumpingCholla = document.getElementById("jumpingcholla");
+  let caneCholla = document.getElementById("canecholla");
+  let teddyBearCholla = document.getElementById("teddybearcholla");
+  
+  let chollaQuestionText = `It is now ${chollaSeason}. There are blooming flowers on some of these cacti. What do the flowers look like?`;
+  let jumpingChollaText = "There are white and pink flowers and green fruit"; // June July August
+  let caneChollaTextSpring = "There are magenta / purple blooms"; // April May
+  let caneChollaTextSummer =
+    "There are no flowers, but there are long-lasting yellow fruits"; // June July August
+  let teddyBearChollaText = "There are yellow-green flowers"; // April May June September
 
-(function chollaTime(chollaMonth) {
+  var chollaMonth, chollaSeason;
   var chollaDay = new Date();
   chollaMonth = chollaDay.getUTCMonth();
-  
-  if (chollaMonth >= 3 && chollaMonth <=8) {
-    switch(chollaMonth) {
-      case 3 : 
-        chollaMonth = "April";
-        return chollaMonth;
-      break;
-      case 4 : 
-        chollaMonth = "May";
-        return chollaMonth;
-      break;
-      case 5 : 
-        chollaMonth = "June";
-        return chollaMonth;
-      break;
-      case 6 : 
-        chollaMonth = "July";
-        return chollaMonth;
-      break;
-      case 7 : 
-        chollaMonth = "August";
-        return chollaMonth;
-      break;
-      case 8 :
-        chollaMonth = "September";
-        return chollaMonth;
-      break;
-      default :
-        return chollaMonth;
-    }
 
+  if (chollaMonth === 3 || 4) { // April and May
+    chollaSeason = "spring";
+    switchText(chollaQuestion, chollaQuestionText);
+    switchText(caneCholla, caneChollaTextSpring);
+    switchText(teddyBearCholla, teddyBearChollaText);
+  } else if (chollaMonth === 5) { // June
+    chollaSeason = "June";
+    switchText(chollaQuestion, chollaQuestionText);
+    switchText(jumpingCholla, jumpingChollaText);
+    switchText(caneCholla, caneChollaTextSummer);
+    switchText(teddyBearCholla, teddyBearChollaText);
+  } else if (chollaMonth === 6 || 7) { // July and August
+    chollaSeason = "summer";
+    switchText(chollaQuestion, chollaQuestionText);
+    switchText(jumpingCholla, jumpingChollaText);
+    switchText(caneCholla, caneChollaTextSummer);
+  } else if (chollaMonth === 8) { // September
+    chollaSeason = "September";
+    switchText(chollaQuestion, chollaQuestionText);
+    switchText(chollaQuestion, chollaQuestionText);
+    switchText(teddyBearCholla, teddyBearChollaText);
+  } else {
+    switchText(chollaQuestion, "These cacti have distinct needles. What do they look like?");
   }
-})();
-
-function setChollaText(chollaPlant, chollaNewText) {
-  
 }
 
-// if chollaQuestion appears on page, setChollaText(selection);
+const replyForChollaButton = document.getElementById("chollabutton");
 
+replyForChollaButton.addEventListener("click", function() {
+  setChollaText();
+});
 
-/*
+const switchText = (elem, txt) => {
+  elem.innerHTML = txt;
+};
 
-"1B2BA" : {
-    bot_says: [ "We are getting there, but I have another question.",
-                "It is now {time of year - calculate}.",
-                "What do the flowers look like?"
-        ],
-human_reply: [  
-            {
-                human_response: "{June-July-August} There are white and pink flowers and green fruit {default} There are no flowers, but there are needles that are the length of fingers and extremely short",
-                bot_answer: "1B2BAjumpingCholla"
-            },   
-            {
-                human_response: "{April-May-June} There are magenta to purple blooms {July-August} There are no flowers, but there are long-lasting yellow fruits {default} There are no flowers, but this cactus has moderate needles",
-                bot_answer: "1B2BAcaneCholla"
-            },   
-            {
-                human_response: "{April-May-June-September} There are yellow green flowers {default} There are no flowers, but the needles look fuzzy",
-                bot_answer: "1B2BAteddyBearCholla"
-            }
-        ]
-},
-
-*/
-
-
-// fix later
-// tk ~line 470 "1B2BA" - new Date - month shows the answer
-// ^^ have a div with a class and the default value - if time of year (month) change the inner html
-// test that the new Date works in each month April through September and NOT in those months (needles) with setUTCMonth https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/setUTCMonth
+// to do
+// write a test that the chollaDay works April, May, June, July, August, September and NOT in those months with setUTCMonth https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/setUTCMonth
